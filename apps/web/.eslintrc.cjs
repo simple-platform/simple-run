0038a9ad90192d61aac096e7a4d2611ab18e9ebb
Munjal A Patel <2717686+munjalpatel@users.noreply.github.com>
2023-12-22 00:10:50 -0800

    refactor(eslint): resolve issues with eslint config

--- apps/web/.eslintrc.cjs
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  root: true,
-  extends: ["@simple-run/eslint-config/nuxt"],
-};
--- packages/ui/.eslintrc.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  root: true,
-  extends: ["plugin:vue/vue3-essential", "@simple-run/eslint-config/vue"],
-};
