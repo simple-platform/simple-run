fd2c5bae1936277d145c0e7addef93855fdc547b
Munjal A Patel <2717686+munjalpatel@users.noreply.github.com>
2023-12-24 01:56:15 -0800

    refactor(setup): add minimal simple repo url validation

--- apps/web/src/app/setup/page.tsx
+++ apps/web/src/app/setup/page.tsx
@@ -1,31 +1,66 @@
+'use client'
+
 import { ChevronDoubleRightIcon } from '@heroicons/react/24/outline'
 import { Header } from '@simple-run/ui/header'
+import { type ChangeEvent, useState } from 'react'
 
-const SetupForm: JSX.Element = (
-  <section className="max-w-sm md:max-w-xl w-full mt-6 md:mt-12">
-    <div className="p-3 md:px-6 md:py-6 shadow rounded-lg dark:bg-slate-800">
-      <label className="label" htmlFor="github_url">
-        <span className="label-text-alt">
-          GitHub URL
-          {' '}
-          <span className="italic">( we only support public GitHub repos for now )</span>
-        </span>
-      </label>
-      <div className="join w-full">
-        <input className="input input-bordered w-full join-item" name="github_url" placeholder="https://github.com/simple-platform/simple-run" type="text" />
-        <button className="btn join-item input-bordered border-l-0">
-          <ChevronDoubleRightIcon className="w-6" />
-        </button>
+function RepoInput(isSmallDevice: boolean) {
+  const placeholder = isSmallDevice
+    ? 'https://github.com/simple...'
+    : 'https://github.com/simple-platform/simple-run'
+
+  const errorMessage = 'We are unable to load details about this repository.'
+  const [error, setError] = useState('')
+
+  function handleRepoUrlChange(e: ChangeEvent<HTMLInputElement>) {
+    const repoUrl = e.target.value.trim().toLowerCase()
+
+    if (repoUrl === '')
+      return
+
+    if (!repoUrl.startsWith('https://'))
+      setError(errorMessage)
+
+    // @todo: call github
+  }
+
+  return (
+    <section className="max-w-sm md:max-w-2xl w-full mt-6 md:mt-12">
+      <div className="p-3 md:px-6 md:py-6 shadow rounded-lg bg-slate-200 dark:bg-slate-800">
+        <label className="label" htmlFor="github_url">
+          <span className="label-text-alt">
+            GitHub URL
+            {' '}
+            <span className="italic">
+              ( we only support
+              {' '}
+              <span className="underline underline-offset-2 decoration-dotted">public</span>
+              {' '}
+              repos for now )
+            </span>
+          </span>
+        </label>
+        <div className="join w-full">
+          <input autoComplete="off" className="input input-bordered w-full join-item" name="github_url" onBlur={handleRepoUrlChange} onFocus={() => setError('')} placeholder={placeholder} type="text" />
+          <button className="btn join-item input-bordered border-l-0">
+            <ChevronDoubleRightIcon className="w-6" />
+          </button>
+        </div>
+        <span className="text-sm italic text-red-600 transition" hidden={error === ''}>{error}</span>
       </div>
-    </div>
-  </section>
-)
+    </section>
+  )
+}
 
 export default function Page(): JSX.Element {
+  // @todo: properly implement using media query
+  const isSmallDevice = false
+  const title = isSmallDevice ? 'Let\'s setup your project!' : 'Let\'s setup your project to run locally!'
+
   return (
     <main className="h-full flex flex-col justify-center items-center p-6 md:p-12">
-      <Header className="text-center" title="Let's setup your project to run locally!" />
-      {SetupForm}
+      <Header className="text-center max-w-3xl" title={title} />
+      {RepoInput(false)}
     </main>
   )
 }
--- packages/ui/src/header.tsx
+++ packages/ui/src/header.tsx
@@ -1,6 +1,6 @@
 export function Header({ className, title }: { className?: string, title: string }) {
   return (
-    <header className={`prose tracking-wider text-sm md:text-md ${className}`}>
+    <header className={`prose tracking-wider text-xs md:text-lg ${className}`}>
       <h1>{title}</h1>
     </header>
   )
