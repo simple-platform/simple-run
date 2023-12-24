2300a6cc04bb9f4044848e77254a2ed6d9847fe4
Munjal A Patel <2717686+munjalpatel@users.noreply.github.com>
2023-12-23 21:47:41 -0800

    refactor(styles): update font styles and setup tailwind with daisy-ui (#15)
    * refactor(web): remove template boilerplate
    
    * refactor(fonts): update setup to use inter variable fonts
    
    * refactor(styles): add heading styles with outfit fonts and update tailwind color config
    
    * refactor(styles): add and configure daisyui
    
    * refactor(styles): update light and dark themes

--- packages/tailwind-config/tailwind.config.ts
+++ packages/tailwind-config/tailwind.config.ts
@@ -1,14 +1,43 @@
 import type { Config } from 'tailwindcss'
 
+import forms from '@tailwindcss/forms'
+import typography from '@tailwindcss/typography'
+import daisyui from 'daisyui'
+import twc from 'tailwindcss/colors'
+import defaultTheme from 'tailwindcss/defaultTheme'
+
+// eslint-disable-next-line unused-imports/no-unused-vars
+const { blueGray, coolGray, lightBlue, trueGray, warmGray, ...colors } = twc
+
 // We want each package to be responsible for its own content.
 const config: Omit<Config, 'content'> = {
-  plugins: [],
+  daisyui: {
+    base: true, // applies background color and foreground color for root element by default
+    darkTheme: 'night', // name of one of the included themes for dark mode
+    logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
+    prefix: '', // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
+    styled: true, // include daisyUI colors and design decisions for all components
+    themeRoot: ':root', // The element that receives theme color CSS variables
+    themes: ['bumblebee', 'night'], // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
+    utils: true, // adds responsive and modifier utility classes
+  },
+
+  darkMode: 'media',
+
+  jit: true,
+
+  plugins: [
+    daisyui,
+    typography,
+    forms,
+  ],
 
   theme: {
+    colors,
+
     extend: {
-      backgroundImage: {
-        'glow-conic':
-          'conic-gradient(from 180deg at 50% 50%, #2a8af6 0deg, #a853ba 180deg, #e92a67 360deg)',
+      fontFamily: {
+        sans: ['InterVariable', ...defaultTheme.fontFamily.sans],
       },
     },
   },
--- packages/ui/tailwind.config.ts
+++ packages/ui/tailwind.config.ts
@@ -2,9 +2,8 @@ import type { Config } from 'tailwindcss'
 
 import sharedConfig from '@simple-run/tailwind-config'
 
-const config: Pick<Config, 'content' | 'prefix' | 'presets'> = {
+const config: Pick<Config, 'content' | 'presets'> = {
   content: ['./src/**/*.tsx'],
-  prefix: 'ui-',
   presets: [sharedConfig],
 }
 
