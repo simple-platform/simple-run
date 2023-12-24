d64298e62b3979248cc0263263bd7a383aca1325
Munjal A Patel <2717686+munjalpatel@users.noreply.github.com>
2023-12-23 21:33:37 -0800

    refactor(styles): add and configure daisyui

--- packages/tailwind-config/tailwind.config.ts
+++ packages/tailwind-config/tailwind.config.ts
@@ -1,5 +1,8 @@
 import type { Config } from 'tailwindcss'
 
+import forms from '@tailwindcss/forms'
+import typography from '@tailwindcss/typography'
+import daisyui from 'daisyui'
 import twc from 'tailwindcss/colors'
 import defaultTheme from 'tailwindcss/defaultTheme'
 
@@ -16,9 +19,24 @@ const colorMap: any = {
 
 // We want each package to be responsible for its own content.
 const config: Omit<Config, 'content'> = {
+  daisyui: {
+    base: true, // applies background color and foreground color for root element by default
+    darkTheme: 'dark', // name of one of the included themes for dark mode
+    logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
+    prefix: '', // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
+    styled: true, // include daisyUI colors and design decisions for all components
+    themeRoot: ':root', // The element that receives theme color CSS variables
+    themes: false, // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
+    utils: true, // adds responsive and modifier utility classes
+  },
+
   jit: true,
 
-  plugins: [],
+  plugins: [
+    daisyui,
+    typography,
+    forms,
+  ],
 
   theme: {
     colors: {
