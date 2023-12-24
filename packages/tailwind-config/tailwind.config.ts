2670426e51af9b16b61b0c56674c2029b9a99600
Munjal A Patel <2717686+munjalpatel@users.noreply.github.com>
2023-12-23 21:44:25 -0800

    refactor(styles): update light and dark themes

--- packages/tailwind-config/tailwind.config.ts
+++ packages/tailwind-config/tailwind.config.ts
@@ -9,26 +9,20 @@ import defaultTheme from 'tailwindcss/defaultTheme'
 // eslint-disable-next-line unused-imports/no-unused-vars
 const { blueGray, coolGray, lightBlue, trueGray, warmGray, ...colors } = twc
 
-const colorMap: any = {
-  danger: 'red',
-  info: 'blue',
-  primary: 'indigo',
-  success: 'green',
-  warning: 'yellow',
-}
-
 // We want each package to be responsible for its own content.
 const config: Omit<Config, 'content'> = {
   daisyui: {
     base: true, // applies background color and foreground color for root element by default
-    darkTheme: 'dark', // name of one of the included themes for dark mode
+    darkTheme: 'night', // name of one of the included themes for dark mode
     logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
     prefix: '', // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
     styled: true, // include daisyUI colors and design decisions for all components
     themeRoot: ':root', // The element that receives theme color CSS variables
-    themes: false, // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
+    themes: ['bumblebee', 'night'], // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
     utils: true, // adds responsive and modifier utility classes
   },
+
+  darkMode: 'media',
 
   jit: true,
 
@@ -39,14 +33,7 @@ const config: Omit<Config, 'content'> = {
   ],
 
   theme: {
-    colors: {
-      ...colors,
-      ...Object.keys(colorMap).map((color) => {
-        const kvp: any = {}
-        kvp[color] = colorMap[color]
-        return kvp
-      }),
-    },
+    colors,
 
     extend: {
       fontFamily: {
