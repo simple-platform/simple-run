293c70c5b6beeca04e32ebd2d48689c7705fa016
Munjal A Patel <2717686+munjalpatel@users.noreply.github.com>
2023-12-24 19:53:13 -0800

    chore(deps): add credo as a dependency for the actions project

--- apps/actions/mix.exs
+++ apps/actions/mix.exs
@@ -41,7 +41,8 @@ defmodule Actions.MixProject do
       {:telemetry_metrics, "~> 0.6"},
       {:telemetry_poller, "~> 1.0"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
     ]
   end
 
