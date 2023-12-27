b7411fa13e220b39556380ccf47f0ccdf8d01db3
Munjal A Patel <2717686+munjalpatel@users.noreply.github.com>
2023-12-27 07:30:19 -0800

    feat(web): add ability for users to add code to clipboard

--- apps/web/src/app/setup/simple-run-code.tsx
+++ apps/web/src/app/setup/simple-run-code.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { ClipboardDocumentListIcon } from '@heroicons/react/24/outline'
 import Markdown from 'react-markdown'
 import rehypeRaw from 'rehype-raw'
 
@@ -44,10 +45,11 @@ export function SimpleRunCode() {
           and you&apos;ll be all set!
         </div>
         <div className="flex flex-col md:flex-row space-y-3 md:space-y-0 md:space-x-6">
-          <div className="w-full">
-            <pre className="bg-slate-950 rounded-lg whitespace-normal p-3 md:p-6">
-              <code className="text-sm">{code}</code>
-            </pre>
+          <div className="w-full flex bg-slate-950 rounded-lg">
+            <code className="p-3 md:p-6 text-sm break-all">{code}</code>
+            <div>
+              <ClipboardDocumentListIcon className="w-4 h-4 relative right-3 top-3 cursor-pointer hover:text-orange-300 active:text-indigo-300" onClick={() => navigator.clipboard.writeText(code)} />
+            </div>
           </div>
           <div className="flex items-center justify-center bg-slate-50 dark:bg-slate-950 p-3 md:p-6 rounded-lg">
             <Markdown className="block text-center markdown" rehypePlugins={[rehypeRaw]}>{md}</Markdown>
