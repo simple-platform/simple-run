aa92bcf88fab29ba37f5e2f24daa5a153e9f1ca6
Munjal A Patel <2717686+munjalpatel@users.noreply.github.com>
2023-12-30 14:39:03 -0800

    chore(agent): add boilerplate for agent using tauri (#42)
    * chore(agent): remove wails-based agent app
    
    * chore(agent): add boilerplate for agent using tauri

--- apps/agent/next.config.js
+++ apps/agent/next.config.js
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 
 module.exports = {
-  distDir: '../agent/frontend/dist',
   output: 'export',
 
-  poweredByHeader: false,
   reactStrictMode: true,
 
   transpilePackages: ['@simple-run/ui'],
