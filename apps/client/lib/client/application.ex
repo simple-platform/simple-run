f27dcb7594903e3c62610f4a5d86fdae7eaa5352
Munjal A Patel <2717686+munjalpatel@users.noreply.github.com>
2024-01-01 14:39:42 -0800

    refactor(client): add boilerplate for simplerun client

---
+++ apps/client/lib/client/application.ex
@@ -0,0 +1,33 @@
+defmodule Client.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      Client.Telemetry,
+      # Start a worker by calling: Client.Worker.start_link(arg)
+      # {Client.Worker, arg},
+      # Start to serve requests, typically the last entry
+      Client.Endpoint,
+
+      {Phoenix.PubSub, name: Client.PubSub}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Client.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  # Tell Phoenix to update the endpoint configuration
+  # whenever the application is updated.
+  @impl true
+  def config_change(changed, _new, removed) do
+    Client.Endpoint.config_change(changed, removed)
+    :ok
+  end
+end
--- config/config.exs
+++ config/config.exs
@@ -9,6 +9,42 @@
 # move said applications out of the umbrella.
 import Config
 
+config :client,
+  generators: [context_app: false]
+
+# Configures the endpoint
+config :client, Client.Endpoint,
+  url: [host: "localhost"],
+  adapter: Phoenix.Endpoint.Cowboy2Adapter,
+  render_errors: [
+    formats: [html: Client.ErrorHTML, json: Client.ErrorJSON],
+    layout: false
+  ],
+  pubsub_server: Client.PubSub,
+  live_view: [signing_salt: "MEho23Jd"]
+
+# Configure esbuild (the version is required)
+config :esbuild,
+  version: "0.19.11",
+  default: [
+    args:
+      ~w(js/app.js --bundle --target=es2017 --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),
+    cd: Path.expand("../apps/client/assets", __DIR__),
+    env: %{"NODE_PATH" => Path.expand("../deps", __DIR__)}
+  ]
+
+# Configure tailwind (the version is required)
+config :tailwind,
+  version: "3.3.5",
+  default: [
+    args: ~w(
+      --config=tailwind.config.js
+      --input=css/app.css
+      --output=../priv/static/assets/app.css
+    ),
+    cd: Path.expand("../apps/client/assets", __DIR__)
+  ]
+
 web_endpoint = System.get_env("SIMPLE_RUN_WEB_ENDPOINT", "http://localhost:3000")
 
 config :actions,
