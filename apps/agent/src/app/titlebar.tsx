ed0c930233ded240ab2afc94deeeca2f77930d8c
Munjal A Patel <2717686+munjalpatel@users.noreply.github.com>
2024-01-01 10:22:29 -0800

    chore(agent): remove next based agent

--- apps/agent/src/app/titlebar.tsx
+++
@@ -1,3 +0,0 @@
-export default function Titlebar() {
-  return <section className="text-sm font-semibold flex items-center justify-center h-7 fixed w-full" data-tauri-drag-region>Simple Run</section>
-}
