782e06435ec42ba6d571a4ed75ef84285cd2b634
Munjal A Patel <2717686+munjalpatel@users.noreply.github.com>
2024-01-01 23:54:52 -0800

    refactor(client): resolve client production run issues

--- apps/client/src-tauri/src/main.rs
+++ apps/client/src-tauri/src/main.rs
@@ -1,6 +1,13 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+use base64::Engine;
+use rand::{distributions::Standard, Rng};
 use tauri::api::process::{Command, CommandEvent};
+
+fn generate_secret_key_base() -> String {
+    let random_bytes: Vec<u8> = rand::thread_rng().sample_iter(Standard).take(64).collect();
+    base64::engine::general_purpose::STANDARD_NO_PAD.encode(random_bytes)
+}
 
 fn main() {
     tauri::Builder::default()
@@ -16,6 +23,7 @@ fn start_server() {
     tauri::async_runtime::spawn(async move {
         let (mut rx, mut _child) = Command::new_sidecar("desktop")
             .expect("failed to setup `desktop` sidecar")
+            .args(["--client", &generate_secret_key_base()])
             .spawn()
             .expect("Failed to spawn packaged node");
 
@@ -29,7 +37,7 @@ fn start_server() {
 
 fn check_server_started() {
     let sleep_interval = std::time::Duration::from_millis(200);
-    let host = "localhost".to_string();
+    let host = "127.0.0.1".to_string();
     let port = "5000".to_string();
     let addr = format!("{}:{}", host, port);
     println!(
@@ -38,9 +46,8 @@ fn check_server_started() {
     );
     loop {
         if std::net::TcpStream::connect(addr.clone()).is_ok() {
-           break;
+            break;
         }
         std::thread::sleep(sleep_interval);
     }
 }
-
