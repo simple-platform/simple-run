44ec79751b84e61a28f302459e2110dcc82b3465
Munjal A Patel <2717686+munjalpatel@users.noreply.github.com>
2024-01-02 23:52:16 -0800

    chore(client): add client-core

---
+++ apps/client_core/lib/client_core.ex
@@ -0,0 +1,18 @@
+defmodule ClientCore do
+  @moduledoc """
+  Documentation for `ClientCore`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ClientCore.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
